6ef9e4aaab6c6a78f8887cf03a49fe4025d746ee
AdrianBaron12 <sebastian.baron29@outlook.com>
2023-07-05 17:14:38 +0300

    Update 1
    Calculator added

--- week40HW/week40HW/week40HW/Exercise4/Calculator.cs
+++ week40HW/week40HW/week40HW/Exercise4/Calculator.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace week40HW.Exercise4
+namespace Week40.Calculator1
 {
-    internal class Calculator
+    public class Calculator
     {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            try
+            {
+                return x / y;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
+
+        public double Power(double x, double y)
+        {
+            return Math.Pow(x, y);
+        }
+
+        public double SquareRoot(double x)
+        {
+            return Math.Sqrt(x);
+        }
+
+
+
     }
 }
--- week40HW/week40HW/week40HW/Exercise6&7/StudentCollegeProfessor.cs
+++ week40HW/week40HW/week40HW/Exercise6&7/StudentCollegeProfessor.cs
@@ -3,10 +3,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Week40.Faculty1;
 
-namespace week40HW.Exercise6_7
+namespace Week40.Faculty2
 {
-    internal class StudentCollegeProfessor
+    public class College1
     {
+        public string Name { get; set; }
+        public int FounderYear { get; set; }
+        public string City { get; set; }
+
+        public College1()
+        {
+            Name = "Unknown";
+            FounderYear = 0;
+            City = "Unknown";
+        }
+    }
+
+    public class Student1
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public College1 College { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Student1(string name, DateTime startDate, DateTime endDate, College1 college, DateTime birthDate)
+        {
+            Name = name;
+            StartDate = startDate;
+            EndDate = endDate;
+            College = college;
+            BirthDate = birthDate;
+        }
+
+        public bool IsStillStudent()
+        {
+            return DateTime.Today > EndDate ? true : false;
+        }
+
+        public int GetAge()
+        {
+            DateTime Now = DateTime.Now;
+
+            return Convert.ToInt32(Now.Year - BirthDate.Year);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Start Date: {StartDate.ToShortTimeString}");
+            Console.WriteLine($"End Date: {EndDate.ToShortTimeString}");
+            Console.WriteLine($"College: {College.Name}");
+            Console.WriteLine($"Birth Date: {BirthDate.ToShortTimeString}");
+            Console.WriteLine($"Is still a student: {IsStillStudent}");
+            Console.WriteLine($"Age: {GetAge()}");
+        }
+
+    }
+
+    public class Professor1
+    {
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+        public string Specialization { get; set; }
+        public List<Student1> StudentsWhoGiveTheirDegree { get; set; }
+
+        public Professor1(string name, string faculty, string specialization)
+        {
+            Name = name;
+            Faculty = faculty;
+            Specialization = specialization;
+            StudentsWhoGiveTheirDegree = new List<Student1>();
+        }
+
+        public Professor1(string name, string faculty, string specialization, List<Student1> studentsWhoGiveTheirDegree)
+        {
+            Name = name;
+            Faculty = faculty;
+            Specialization = specialization;
+            StudentsWhoGiveTheirDegree = studentsWhoGiveTheirDegree;
+        }
+
+
     }
 }
